97e3dd929764801557e19b25520e65f13c41b8de
Vasiliy0209 <54793048+Vasiliy0209@users.noreply.github.com>
2019-09-12 16:29:21 +0500

    Add files via upload

---
+++ Messenger/Messenger/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Messenger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb_Post = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tb_Subject = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tb_Post
+            // 
+            this.tb_Post.Location = new System.Drawing.Point(22, 89);
+            this.tb_Post.Multiline = true;
+            this.tb_Post.Name = "tb_Post";
+            this.tb_Post.Size = new System.Drawing.Size(526, 315);
+            this.tb_Post.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(139, 434);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(285, 58);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Опубликовать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tb_Subject
+            // 
+            this.tb_Subject.Location = new System.Drawing.Point(22, 30);
+            this.tb_Subject.Name = "tb_Subject";
+            this.tb_Subject.Size = new System.Drawing.Size(526, 22);
+            this.tb_Subject.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(581, 525);
+            this.Controls.Add(this.tb_Subject);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tb_Post);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_Post;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tb_Subject;
+    }
+}
+
---
+++ Messenger/Messenger/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CookComputing.XmlRpc;
+
+namespace Messenger
+{
+    public partial class Form1 : Form
+    {
+        public struct LJPostEventArg
+        {
+            public string username;
+            public string password;
+            [XmlRpcMember("event")]
+            public string ljevent;
+            public string lineendings;
+            public string subject;
+            public int year;
+            public int mon;
+            public int day;
+            public int hour;
+            public int min;
+        }
+
+        public struct LJRetVal
+        {
+            public int itemid;
+            public int anum;
+            public string url;
+        }
+
+        [XmlRpcUrl("http://www.livejournal.com/interface/xmlrpc")]
+        public interface ILJService
+        {
+            [XmlRpcMethod("LJ.XMLRPC.postevent")]
+            LJRetVal postevent(LJPostEventArg posteventarg);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            YandexSpeller.SpellService cl = new YandexSpeller.SpellService();
+            YandexSpeller.SpellError[] errors = cl.checkText(tb_Post.Text,"ru",0,"");
+
+            if(errors.Length !=0)
+            {
+                string message = "", variants = "";
+                foreach (YandexSpeller.SpellError err in errors)
+                {
+                    foreach (string variant in err.s)
+                        variants += variant + "\n";
+                    message += "Слово [" + err.word + "]\nВозможные варианты:\n" + variants;
+                }
+                //Вывод сообщения пользователю
+                MessageBox.Show(message);
+                MessageBox.Show("Плохо писать нехорошо");
+                return;
+            }
+
+            ILJService service = XmlRpcProxyGen.Create<ILJService>();
+            LJPostEventArg arg = new LJPostEventArg();
+
+            arg.day = DateTime.Now.Day;
+            arg.hour = DateTime.Now.Hour;
+            arg.min = DateTime.Now.Minute;
+            arg.mon = DateTime.Now.Month;
+            arg.year = DateTime.Now.Year;
+
+            arg.username = "inf_study";
+            arg.password = "do520xLpim4";
+            arg.lineendings = "pc";
+
+            arg.subject = tb_Subject.Text;
+            arg.ljevent = tb_Post.Text;
+
+            try
+            {
+                LJRetVal ret = service.postevent(arg);
+                MessageBox.Show("Сообщение успешно опубликовано [" + ret.url + "]");
+            }
+            catch(XmlRpcIllFormedXmlException ex)
+            {
+                MessageBox.Show("Сообщение не опубликованно. Ошибка [" + ex.Message + "]");
+            }
+
+            
+            
+            /*catch()
+            {
+                MessageBox.Show("Сообщение не опубликованно. Неизвестная ошибка.");
+            }*/
+
+        }
+    }
+}
